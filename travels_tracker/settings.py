853416cd36fd5aaa8366fdb9f13d8015cdfee475
reed <reed@pogi.com>
2023-09-08 21:45:01 +0800

    added css and finished the project

--- travels_tracker/settings.py
+++ travels_tracker/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party apps
+    "crispy_forms",
+    "crispy_bootstrap5",
     # local apps
     "trips",
 ]
@@ -125,6 +128,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -138,3 +145,9 @@ MEDIA_ROOT = BASE_DIR / "uploads"
 # where sould the user go after login
 
 LOGIN_REDIRECT_URL = "trips-list"
+
+# crispy forms
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
